61ca230bb98d9d7e9b3afb00bf70b4e57b6f98c8
“Raffaella <raffaella.florissi@gmail.com>
2024-05-13 14:02:54 +0100

    add footer and errorpage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 
 
 function Footer(){
-    return(
-        <>
-        <a href="https://github.com/purrgramers/catchonka"> This is the footer </a>
-        </>
+    return (
+        <footer class="footer footer-center  w-full p-4 bg-green-200 text-pink-800">
+            <div class="text-center">
+                <p>
+                    Copyright © 2024 - 
+                    <a class="font-semibold" href="https://github.com/purrgramers/catchonka"
+                    > Catchonka Github</a>
+                </p>
+            </div>
+        </footer>
     )
 }
 
