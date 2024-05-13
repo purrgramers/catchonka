d1ba313e007d31657f71a1293b93bbadac1fabcb
AValenzue <zuellagraphy@gmail.com>
2024-05-13 14:17:18 +0100

    add error page

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile/:catId" element={<Profile />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="/*" element={<ErrorPage />} />
       </Routes>
       <Footer />
     </>
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,4 +1,4 @@
 function ErrorPage(){
-    return
+    return <div><h1>404</h1></div>
 }
 export default ErrorPage
