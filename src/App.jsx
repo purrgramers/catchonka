8b5718d5445fb7ff03b62072f09a83e86d307baa
AValenzue <zuellagraphy@gmail.com>
2024-05-13 12:51:52 +0100

    add pages and route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import AddProfile from "./components/AddProfile";
-import CatProfileList from "./components/CatProfileList";
-import ErrorPage from "./components/ErrorPage";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ErrorPage from "./pages/ErrorPage";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import SignUp from "./components/SignUp";
 
 import './App.css'
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-    
       <h1>Catchonka</h1>
-  
+      <Routes>
+      <Route path="/home" element={<Home />} />
+        <Route path="/profile/:catId" element={<Profile />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </>
   )
 }
