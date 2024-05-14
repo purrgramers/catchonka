dc9c9c5bd7c60e378ed32b26fd01e9591c81ff93
AValenzue <zuellagraphy@gmail.com>
2024-05-14 17:20:19 +0100

    add hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+function HeroSection(){
+    return(
+        <div className="min-h-96 p-8">
+      <h1 className="text-3xl font-bold" >Feature Catchonka</h1>
+      <span className="text-3xl font-bold" > Purrrrr...</span>
+      <br/>
+      <span className="text-3xl font-bold" > Meoowww..</span>
+
+    </div>
+    )
+}
+
+export default HeroSection
