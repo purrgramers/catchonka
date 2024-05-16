b44efeb79a8a199ab73cdb84ce759e5dcc3782d4
AValenzue <zuellagraphy@gmail.com>
2024-05-16 17:55:03 +0100

    reminder to upload picture before creaing

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -127,9 +127,14 @@ function CreateProfile() {
             {isLoading ? (
               "Uploading"
             ) : (
-              <button type="submit" className="bg-indigo-500 p-2">
-                UPLOAD IMAGE
-              </button>
+              <div>
+                <div className="text-xs text-red-800">
+                  Click Upload Picture before creating your profile!
+                </div>
+                <button type="submit" className="bg-indigo-500 p-2">
+                  UPLOAD IMAGE
+                </button>
+              </div>
             )}
           </p>
         </form>
