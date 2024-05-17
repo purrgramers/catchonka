085ee974688bed2d9531edce7cfe6d0be15802a3
AValenzue <zuellagraphy@gmail.com>
2024-05-17 08:40:50 +0100

    install flowbite react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    ('flowbite/plugin')
-  ],
+    import('flowbite/plugin')
+  ]
 }
+
+
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "./node_modules/flowbite/**/*.js"
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     ('flowbite/plugin')
+//   ],
+// }
 
